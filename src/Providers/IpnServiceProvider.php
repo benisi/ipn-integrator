a2c15ab094efeb628cd343599c660318bfff9481
Benjamin <benjamin.isidahomen@gloopro.com>
2020-05-27 05:31:46 +0100

    add package core files

---
+++ src/Providers/IpnServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Workaholic\IpnIntegrator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class IpnServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
